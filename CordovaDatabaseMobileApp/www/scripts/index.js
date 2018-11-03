2ed4debc10cd3f7946c2c1aee5266377aa711fec
juha Penttinen <juha.penttinen@student.laurea.fi>
2018-11-03 19:28:56 +0200

    version 1

---
+++ CordovaDatabaseMobileApp/www/scripts/index.js
@@ -0,0 +1,20 @@
+﻿//Creating a datbase
+$(document).on("ready", function () {
+    databaseHandler.createDatabase();
+});
+//Adding item in table
+function addProduct() {
+    var name = $("#txtName").val();
+    var quantity = $("#txtPassword").val();
+
+    if (!name) {
+        alert("Username is required");
+    } else {
+        var r = confirm("Name: " + name + "\n" + "Quantity: " + quantity);
+        if (r == true) {
+            productHandler.addProduct(name, quantity);
+            $("#txtName").val("");
+            $("#txtPassword").val("");
+        }
+    }
+}
---
+++ CordovaDatabaseMobileApp/www/scripts/productHandler.js
@@ -0,0 +1,19 @@
+﻿var productHandler = {
+    //Add the record in database, it adds record or row in Web SQL (SQLite)
+    addProduct: function (username, passwords) {
+        databaseHandler.db.transaction(
+            function (tx) {
+                tx.executeSql(
+                    "insert into userManager(username, passwords) values(?, ?)",
+                    [username, passwords],
+                    function (tx, results) { },
+                    function (tx, error) {
+                        console.log("add product error: " + error.message);
+                    }
+                );
+            },
+            function (error) { },
+            function () { }
+        );
+    },
+}
